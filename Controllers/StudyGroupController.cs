4dccd48320a452bd4d02ec792aa62d72ec9d3748
force <force@ARRAN>
2025-03-04 14:16:32 +0200

    Prac2 Complete

--- Controllers/StudyGroupController.cs
+++ Controllers/StudyGroupController.cs
@@ -10,18 +10,19 @@ namespace ArranStudyGroup.Controllers
 {
     public class StudyGroupController : Controller
     {
-        public ActionResult Index()
+        public ActionResult ListOfPeopleIndex()
         {
             var members = new List<StudyGroupMember>
         {
                 //some of the following people are real people just not In INF 272
-            new StudyGroupMember { StudentNumber = "u23655675", FirstName = "Arran", LastName = "Lamond", Email = "u23655675@tuks.co.za"},
-            new StudyGroupMember { StudentNumber = "u24641342", FirstName = "Micheal", LastName = "Neto", Email = "u24641342@tuks.co.za"},
-            new StudyGroupMember { StudentNumber = "u23582724", FirstName = "Vuyo", LastName = "Angura", Email = "u23582724@tuks.co.za"},
-            new StudyGroupMember { StudentNumber = "u23557509", FirstName = "Tanyaradzwa", LastName = "Masara", Email = "u23557509@tuks.co.za"},
-            new StudyGroupMember { StudentNumber = "u04613628", FirstName = "Andreas", LastName = "Bester", Email = "u04613628@tuks.co.za"}
+            new StudyGroupMember { StudentNumber = "u23655675", FirstName = "Arran", LastName = "Lamond", Email = "u23655675@tuks.co.za", myLink = "~/HTML/Arran.html"},
+            new StudyGroupMember { StudentNumber = "u24641342", FirstName = "Micheal", LastName = "Neto", Email = "u24641342@tuks.co.za", myLink = "~/HTML/Michael.html"},
+            new StudyGroupMember { StudentNumber = "u23582724", FirstName = "Vuyo", LastName = "Angura", Email = "u23582724@tuks.co.za", myLink = "~/HTML/Vuyo.html"},
+            new StudyGroupMember { StudentNumber = "u23557509", FirstName = "Tanyaradzwa", LastName = "Masara", Email = "u23557509@tuks.co.za", myLink = "~/HTML/Tanyaradzwa.html"},
+            new StudyGroupMember { StudentNumber = "u04613628", FirstName = "Andreas", LastName = "Bester", Email = "u04613628@tuks.co.za", myLink = "~/HTML/Andreas.html"}
         };
             //send to view XDD
+            ViewBag.Members = members;
             return View(members);
         }
     }
--- Models/StudyGroupMember.cs
+++ Models/StudyGroupMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace ArranStudyGroup.Models
 	public class StudyGroupMember
 	{
         //nromal getters and setters
+        [Display(Name = "Student Number")]
         public string StudentNumber { get; set; }
+        [Display(Name = "Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Surname")]
         public string LastName { get; set; }
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
+        [Display(Name = "Link To Personal Page")]
+        public string myLink { get; set; }
+
     }
 }
